dbf3c5e33a5e3368409d143b23abdbc2adf79726
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2022-06-15 13:55:15 +0100

    first commit

---
+++ autocommiter.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+saved_date=$(tail -n 1 autocommiter_tracker | grep -Eo '(?<|)[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}')
+saved_date_timestamp=$(date -d "${saved_date}" +"%s")
+current_folder=$(pwd)
+
+cd $TARGETFOLDER
+choosen=$(git status -s | sed "s@^[^ ]* @$PWD/@" | tail -1)
+
+if [ -z "$choosen" ]
+then
+    echo "No item to save in the repo"
+else
+    echo "Saving $choosen"
+    today_simplified=$(date +"%Y-%m-%d")
+    today_timestamp=$(date -d "${today_simplified}" +"%s")
+
+    if [ -z "$saved_date" ] || [ $today_timestamp -gt $saved_date_timestamp ];
+    then
+        git add $choosen
+        git commit -m "Saving Kata Solution"
+        git push origin master
+
+        # saving current progress, if a file has been saved today no need to save more
+        cd $current_folder
+        echo "$choosen | $today_simplified" >> autocommiter_tracker
+    else
+        cd $current_folder
+        echo "Do nothing"
+    fi
+fi
+
+# * * * * * /usr/bin/bash /home/mdandre/projects/bash/autocommitter.sh
